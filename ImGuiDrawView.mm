7a664201a2c9ce75a391fb52f7005020a9982602
passedwold-lgtm <passedwold@gmail.com>
2026-04-13 10:32:29 +0700

    Update ImGuiDrawView.mm

--- ImGuiDrawView.mm
+++ ImGuiDrawView.mm
@@ -5,34 +5,46 @@
 #import "Esp/ImGuiDrawView.h"
 #import "IMGUI/imgui.h"
 #import "IMGUI/imgui_impl_metal.h"
-#include "font.h"
-#import "Helper/Mem.h"
-//#import "Helper/Hooks.h"
 
 #define kWidth  [UIScreen mainScreen].bounds.size.width
 #define kHeight [UIScreen mainScreen].bounds.size.height
 
 static bool MenDeal = true;
-static bool PC = true;
+
+// ตัวแปร Vars จำลอง (สำหรับ build ผ่าน)
+struct MenuVars {
+    bool Enable = false;
+    bool Box = false;
+    bool skeleton = false;
+    bool lines = false;
+    bool Name = false;
+    bool Health = false;
+    bool Distance = false;
+    bool ShowGhostButton = false;
+    bool ShowTeleKillButton = false;
+    bool ShowTeleVerticalButton = false;
+    bool ShowWallHackButton = false;
+    bool TeleEnmy = false;
+    bool Telekill = false;
+    bool Ninjarun = false;
+} Vars;
 
 @interface ImGuiDrawView () <MTKViewDelegate>
 @property (nonatomic, strong) id <MTLDevice> device;
 @property (nonatomic, strong) id <MTLCommandQueue> commandQueue;
-@property (nonatomic, strong) UIButton *ghostButtonView;
-@property (nonatomic, strong) UISwitch *ghostSwitch;
-@property (nonatomic, strong) UIButton *wallHackButtonView;
-@property (nonatomic, strong) UISwitch *wallHackSwitch;
 @property (nonatomic, strong) UIButton *teleKillButton;
 @property (nonatomic, strong) UISwitch *teleKillSwitch;
 @property (nonatomic, strong) UIButton *telefuckButton;
 @property (nonatomic, strong) UISwitch *telefuckSwitch;
 @property (nonatomic, strong) UIButton *satooButton;
 @property (nonatomic, strong) UISwitch *satooSwitch;
-@property (nonatomic, assign) CGRect menuBounds;
-@property (nonatomic, assign) BOOL isProcessing;
 @end
 
 @implementation ImGuiDrawView
+
++ (void)showChange:(BOOL)open {
+    MenDeal = open;
+}
 
 - (instancetype)initWithNibName:(nullable NSString *)nibNameOrNil bundle:(nullable NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -42,10 +54,9 @@ static bool PC = true;
         
         IMGUI_CHECKVERSION();
         ImGui::CreateContext();
-        ImGuiIO& io = ImGui::GetIO();
+        ImGui::StyleColorsDark();
         ImGui_ImplMetal_Init(_device);
         
-        ImGui::StyleColorsDark();
         ImGui::GetStyle().WindowRounding = 5.0f;
     }
     return self;
@@ -65,71 +76,37 @@ static bool PC = true;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self createTelegogoButton];
-    [self createTelefuckButton];
-    [self createSatooButton];
+    [self createButtons];
 }
 
-- (CGRect)centeredButtonFrameForIndex:(NSInteger)index {
+- (void)createButtons {
     CGFloat w = 58, h = 78, spacing = 15;
     CGFloat total = (4 * h) + (3 * spacing);
     CGFloat startY = (kHeight - total) / 2;
     CGFloat centerX = kWidth / 2 - w / 2;
-    return CGRectMake(centerX, startY + (index * (h + spacing)), w, h);
-}
-
-- (void)createTelegogoButton {
-    self.teleKillButton = [[UIButton alloc] initWithFrame:[self centeredButtonFrameForIndex:0]];
-    self.teleKillButton.backgroundColor = [UIColor clearColor];
-    [self.teleKillButton addTarget:self action:@selector(buttonDragged:withEvent:) forControlEvents:UIControlEventTouchDragInside];
-    [[UIApplication sharedApplication].keyWindow addSubview:self.teleKillButton];
     
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(7, 1, 72, 20)];
-    label.text = @"TeleKill";
-    label.textColor = [UIColor whiteColor];
-    label.font = [UIFont fontWithName:@"AmericanTypewriter-Bold" size:8];
-    [self.teleKillButton addSubview:label];
-    
-    self.teleKillSwitch = [[UISwitch alloc] initWithFrame:CGRectMake(3.5, 20, 51, 31)];
-    self.teleKillSwitch.onTintColor = [UIColor redColor];
-    [self.teleKillSwitch addTarget:self action:@selector(teleKillSwitchChanged:) forControlEvents:UIControlEventValueChanged];
-    [self.teleKillButton addSubview:self.teleKillSwitch];
-}
-
-- (void)createTelefuckButton {
-    self.telefuckButton = [[UIButton alloc] initWithFrame:[self centeredButtonFrameForIndex:2]];
-    self.telefuckButton.backgroundColor = [UIColor clearColor];
-    [self.telefuckButton addTarget:self action:@selector(buttonDragged:withEvent:) forControlEvents:UIControlEventTouchDragInside];
-    [[UIApplication sharedApplication].keyWindow addSubview:self.telefuckButton];
-    
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(7, 1, 72, 20)];
-    label.text = @"NinjaRun";
-    label.textColor = [UIColor whiteColor];
-    label.font = [UIFont fontWithName:@"AmericanTypewriter-Bold" size:8];
-    [self.telefuckButton addSubview:label];
-    
-    self.telefuckSwitch = [[UISwitch alloc] initWithFrame:CGRectMake(3.5, 20, 51, 31)];
-    self.telefuckSwitch.onTintColor = [UIColor purpleColor];
-    [self.telefuckSwitch addTarget:self action:@selector(telefuckSwitchChanged:) forControlEvents:UIControlEventValueChanged];
-    [self.telefuckButton addSubview:self.telefuckSwitch];
-}
-
-- (void)createSatooButton {
-    self.satooButton = [[UIButton alloc] initWithFrame:[self centeredButtonFrameForIndex:3]];
-    self.satooButton.backgroundColor = [UIColor clearColor];
-    [self.satooButton addTarget:self action:@selector(buttonDragged:withEvent:) forControlEvents:UIControlEventTouchDragInside];
-    [[UIApplication sharedApplication].keyWindow addSubview:self.satooButton];
-    
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(7, 1, 72, 20)];
-    label.text = @"Speed";
-    label.textColor = [UIColor whiteColor];
-    label.font = [UIFont fontWithName:@"AmericanTypewriter-Bold" size:8];
-    [self.satooButton addSubview:label];
-    
-    self.satooSwitch = [[UISwitch alloc] initWithFrame:CGRectMake(3.5, 20, 51, 31)];
-    self.satooSwitch.onTintColor = [UIColor greenColor];
-    [self.satooSwitch addTarget:self action:@selector(satooSwitchChanged:) forControlEvents:UIControlEventValueChanged];
-    [self.satooButton addSubview:self.satooSwitch];
+    NSArray *titles = @[@"TeleKill", @"NinjaRun", @"Speed"];
+    for (int i = 0; i < 3; i++) {
+        UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(centerX, startY + (i * (h + spacing)), w, h)];
+        btn.backgroundColor = [UIColor clearColor];
+        [btn addTarget:self action:@selector(buttonDragged:withEvent:) forControlEvents:UIControlEventTouchDragInside];
+        [[UIApplication sharedApplication].keyWindow addSubview:btn];
+        
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(7, 1, 72, 20)];
+        label.text = titles[i];
+        label.textColor = [UIColor whiteColor];
+        label.font = [UIFont fontWithName:@"AmericanTypewriter-Bold" size:8];
+        [btn addSubview:label];
+        
+        UISwitch *sw = [[UISwitch alloc] initWithFrame:CGRectMake(3.5, 20, 51, 31)];
+        sw.onTintColor = i == 0 ? [UIColor redColor] : (i == 1 ? [UIColor purpleColor] : [UIColor greenColor]);
+        [sw addTarget:self action:@selector(switchChanged:) forControlEvents:UIControlEventValueChanged];
+        [btn addSubview:sw];
+        
+        if (i == 0) { self.teleKillButton = btn; self.teleKillSwitch = sw; }
+        else if (i == 1) { self.telefuckButton = btn; self.telefuckSwitch = sw; }
+        else { self.satooButton = btn; self.satooSwitch = sw; }
+    }
 }
 
 - (void)buttonDragged:(UIButton *)sender withEvent:(UIEvent *)event {
@@ -138,9 +115,10 @@ static bool PC = true;
     sender.center = point;
 }
 
-- (void)teleKillSwitchChanged:(UISwitch *)sender { Vars.Telekill = sender.on; }
-- (void)telefuckSwitchChanged:(UISwitch *)sender { Vars.Ninjarun = sender.on; }
-- (void)satooSwitchChanged:(UISwitch *)sender { }
+- (void)switchChanged:(UISwitch *)sender {
+    if (sender == self.teleKillSwitch) Vars.Telekill = sender.on;
+    else if (sender == self.telefuckSwitch) Vars.Ninjarun = sender.on;
+}
 
 #pragma mark - MTKViewDelegate
 
@@ -182,7 +160,6 @@ static bool PC = true;
             }
             if (ImGui::BeginTabItem("Extra")) {
                 ImGui::Checkbox("Tele Enemy", &Vars.TeleEnmy);
-                ImGui::Checkbox("Bypass PC", &PC);
                 ImGui::EndTabItem();
             }
             ImGui::EndTabBar();
